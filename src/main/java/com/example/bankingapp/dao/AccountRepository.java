c9c0f8171d9263f86efce1f3e5f3b76c2244980f
himanshu-thakur-7 <himan7781@gmail.com>
2023-08-10 10:25:23 +0530

    file heirarchy updated

--- src/main/java/com/example/bankingapp/dao/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bankingapp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bankingapp.model.Account;
-
-public interface AccountRepository extends  JpaRepository <Account,String> {
-
-}
--- src/main/java/com/example/bankingapp/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bankingapp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bankingapp.model.Customer;
-
-public interface CustomerRepository extends JpaRepository <Customer,String> {
-
-}
