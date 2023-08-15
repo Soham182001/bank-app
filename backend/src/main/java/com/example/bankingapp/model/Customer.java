ce3561f122fdcdb816ae5dac63d7df0bb9aab8ee
himanshu-thakur-7 <himan7781@gmail.com>
2023-08-15 13:00:10 +0530

    updated customer schema

--- backend/src/main/java/com/example/bankingapp/model/Customer.java
+++ backend/src/main/java/com/example/bankingapp/model/Customer.java
@@ -23,27 +23,40 @@ import jakarta.validation.constraints.Size;
 @Table(name = "Customer_info")
 public class Customer {
 
+	// Customer Fields
 	@Id
 	@NotBlank(message = "Customer ID can't be blank")
 	@Length(min = 7, max = 15, message = "Customer ID must be between 7 - 15 letters")
 	private String custId;
 
-	@NotBlank(message = "Customer name can't be blank")
-	private String name;
+	@Column(nullable = false)
+	@NotBlank(message = "Customer first name can't be blank")
+	private String firstName;
 
-	@NotBlank(message = "Customer address can't be blank")
-	private String address;
+	private String middleName;
 
+	@Column(nullable = false)
+	@NotBlank(message = "Customer last name can't be blank")
+	private String lastName;
+
+	@Column(nullable = false)
+	@NotBlank(message = "Customer father's name can't be blank")
+	private String fatherName;
+
+	@Column(nullable = false)
 	@NotBlank(message = "Customer phone can't be blank")
 	@Pattern(regexp = "[\\d]{10}")
 	private String phone;
 
+	@Column(nullable = false)
 	@NotBlank(message = "Customer email can't be blank")
 	@Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
-	// @Pattern(regexp="/^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$/",message="Invalid
-	// Email Address")
 	private String email;
 
+	@Column(nullable = false)
+	private Integer adhaarNumber;
+
+	@Column(nullable = false)
 	@NotBlank(message = "Customer password can't be blank")
 	@Size(min = 7, max = 14, message = "Password must be between 7 - 14 characters")
 	private String password;
@@ -55,6 +68,8 @@ public class Customer {
 	@OneToMany(mappedBy = "customer", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	private List<Account> account;
 
+	// Getters and setters
+
 	public String getCustId() {
 		return custId;
 	}
@@ -63,20 +78,36 @@ public class Customer {
 		this.custId = custId;
 	}
 
-	public String getName() {
-		return name;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
-	public String getAddress() {
-		return address;
+	public String getMiddleName() {
+		return middleName;
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFatherName() {
+		return fatherName;
+	}
+
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
 	}
 
 	public String getPhone() {
@@ -93,6 +124,14 @@ public class Customer {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public Integer getAdhaarNumber() {
+		return adhaarNumber;
+	}
+
+	public void setAdhaarNumber(Integer adhaarNumber) {
+		this.adhaarNumber = adhaarNumber;
 	}
 
 	public LocalDate getDOB() {
