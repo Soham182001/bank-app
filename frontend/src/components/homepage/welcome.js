76bde62bbf75d3b3e22a8f9f54cf06d191a71aad
himanshu-thakur-7 <himan7781@gmail.com>
2023-08-17 06:13:43 +0530

    addded transaction page

--- frontend/src/components/homepage/welcome.js
+++ frontend/src/components/homepage/welcome.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import sessionStorage from "sessionstorage";
 
+import {useNavigate} from "react-router-dom"
+import axios from 'axios';
 const WelcomePage = () =>{    
 
+    const navigate = useNavigate();
     let data = sessionStorage.getItem("info");
     data = JSON.parse(data);
 
+    const custId = data.custId;
+
     const handleLogout = () =>{
         
+        
     }
+
+    const saveData = (res) => {
+        sessionStorage.setItem("account", res);
+    }
+
+    const fetchAndMove = ()=>{
+
+        const URL = `http://localhost:8080/fetchAccounts/${custId}`
+        axios({
+            method: 'get',
+            url: URL,
+          })
+        .then(
+            response=>{
+                console.log(response.data);
+                
+                saveData(JSON.stringify(response.data));
+
+                navigate('/transact')
+            }
+        )
+        .catch(e => {
+            alert(e.message);
+            console.log(e);
+        })
+
+        
+        navigate('/transact')
+    }
+
 
     return(
         <div>
@@ -19,6 +55,7 @@ const WelcomePage = () =>{
                 Welcome to Wells Fargo {data.custId}!
             </h1>
             
+            <button onClick={fetchAndMove}>Transact Money</button>
             
             <div>
                 <h2>Have a good day!</h2>
