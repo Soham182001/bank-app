87f861497b352da5be46747917c3c03116988b2f
ashishtayal01 <ashishtayal31@gmail.com>
2023-08-17 14:36:43 +0530

    Fund transfer

--- backend/src/main/java/com/example/bankingapp/service/TransactionService.java
+++ backend/src/main/java/com/example/bankingapp/service/TransactionService.java
@@ -33,22 +33,59 @@ public class TransactionService {
 		
 		trans.setSenderAccount(senderAccount);
 		trans.setRecieverAccount(recieverAccount);
+		trans.setAmount(accountModel.getBalance());
 		
 		String res="";
 
 		if(senderNum.equals(recieverNum)) {
-			int balance=senderAccount.getBalance();
-			if(balance-accountModel.getBalance()<-100) {
-				res="Insufficient Balance";
+			if(trans.getType().equals("withdrawal")) {
+				int balance=senderAccount.getBalance();
+				if(balance-accountModel.getBalance()<0) {
+					res="Insufficient Balance";
+					trans.setStatus("Failed");
+				}
+				else {
+					int rows=accRepo.updateBalance(accountModel.getBalance(),senderNum);
+					if(rows>0) {
+						res="Withdrawal Successful";
+						trans.setStatus("Success");
+					}
+				}
 			}
 			else {
-				int rows=accRepo.updateBalance(accountModel.getBalance(),senderNum);
+				int balance=senderAccount.getBalance();
+				int rows=accRepo.updateBalance1(accountModel.getBalance(),senderNum);
 				if(rows>0) {
-					res="Transaction Successful";
+					res="Deposit Successful";
+					trans.setStatus("Success");
+				}
+				else {
+					res="Error";
+					trans.setStatus("Failed");
+				}
+			}
+			
+		}
+		else {
+			int b1=senderAccount.getBalance();
+			int b2=recieverAccount.getBalance();
+			if(b1-accountModel.getBalance()<0) {
+				res="Insufficient Balance";
+				trans.setStatus("Failed");
+			}
+			else {
+				int rows1=accRepo.updateBalance(accountModel.getBalance(),senderNum);
+				int rows2=accRepo.updateBalance1(accountModel.getBalance(),recieverNum);
+				if(rows1>0 && rows2>0) {
+					res="Fund transferred successfully.";
+					trans.setStatus("Success");
+				}
+				else {
+					res="Transfer failed.";
+					trans.setStatus("Failed");
 				}
 			}
 		}
-		
 		
 		
 		transRepo.save(trans);
