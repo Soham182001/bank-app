d17e3b5b190387e790bf2b7d7122f74824df1503
Manish Mayank <manmayoff@gmail.com>
2023-08-22 14:30:17 +0530

    withdraw page added

--- frontend/src/components/homepage/sideBar.js
+++ frontend/src/components/homepage/sideBar.js
@@ -18,7 +18,7 @@ const Sidebar = () => {
             <CDBSidebarMenuItem icon="user"><a href='/welcome/account'>Account Details</a></CDBSidebarMenuItem>
             <CDBSidebarMenuItem icon="exchange-alt"><a href='/welcome/transact'>Fund Transfer</a></CDBSidebarMenuItem>
             <CDBSidebarMenuItem icon="hourglass" iconType="solid"><a href='/welcome/showTransaction'>Transactions</a></CDBSidebarMenuItem>
-            <CDBSidebarMenuItem icon="credit-card" iconType="solid"><a href='/welcome/transact'>Withdrawal/Deposit</a></CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="credit-card" iconType="solid"><a href='/welcome/withdraw'>Withdrawal/Deposit</a></CDBSidebarMenuItem>
             <CDBSidebarMenuItem icon="money-bill" iconType="solid"><a href='/welcome/showBalance'>Check Balance</a></CDBSidebarMenuItem>
             <CDBSidebarMenuItem icon="plus" iconType="solid"><a href='/welcome/createaccount'>New Account</a></CDBSidebarMenuItem>            
           </CDBSidebarMenu>
