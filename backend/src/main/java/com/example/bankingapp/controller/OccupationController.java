2552d65dcf18db2aa52d11b3b0698e36cc1a7a0d
hilmiparveen <hilmiparveen@gmail.com>
2023-08-23 15:35:27 +0530

    added update occupation

---
+++ backend/src/main/java/com/example/bankingapp/controller/OccupationController.java
@@ -0,0 +1,26 @@
+package com.example.bankingapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bankingapp.model.Address;
+import com.example.bankingapp.model.Occupation;
+import com.example.bankingapp.service.AddressService;
+import com.example.bankingapp.service.OccupationService;
+
+@RestController
+@CrossOrigin("*")
+public class OccupationController {
+	@Autowired
+	OccupationService occuService;
+	@PutMapping("/updateOccupation/{custId}")
+	  public String updateOccupation(@PathVariable("custId") String custId, @RequestBody Occupation occu) {
+		  
+		  return occuService.updateOccupation(custId,occu);
+	  }
+
+}
