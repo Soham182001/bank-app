57a6b0766f7eba39ab1f70d2ad7f0ac9f369ab46
hilmiparveen <hilmiparveen@gmail.com>
2023-08-23 08:48:52 +0530

    added check balance by account number

---
+++ Documents/bank-app-master/frontend/src/components/homepage/welcome.js
@@ -0,0 +1,33 @@
+import React, {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import Content from './content';
+import NavbarBootstrap from './navbar';
+import Sidebar from './sideBar';
+import sessionStorage from "sessionstorage";
+
+const WelcomePage = () =>{
+    
+    const navigate  = useNavigate();
+
+    const checkUser = () => {
+        if(sessionStorage.getItem('info') == null){
+            navigate('/login');
+        }
+    }
+
+    useEffect(()=>{
+        checkUser();
+    },[sessionStorage.getItem('info')])
+
+    return(
+        <div>
+            <NavbarBootstrap/>
+            <div style={{display: "flex", flexDirection: "row"}}>
+                <Sidebar/>
+                <Content/>
+            </div>
+        </div>
+    )
+}
+
+export default WelcomePage;
