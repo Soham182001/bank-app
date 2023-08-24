45ecb51e72bfca2aaeafc03acde582d474140ce1
himanshu-thakur-7 <himan7781@gmail.com>
2023-08-24 06:30:55 +0530

    added check balance admin feature

--- frontend/src/components/showBalance/showBalanceAdmin.js
+++ frontend/src/components/showBalance/showBalanceAdmin.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import Select from 'react-select';
 
+import Container from 'react-bootstrap/Container';
+import { CDBInput, CDBCard, CDBCardBody, CDBBtn, CDBLink, CDBContainer } from 'cdbreact';
+import '../../components/css/components.css'
+
 const ShowBalanceAdmin = () =>{
 
     const [custId, setCustId] = useState("");
@@ -19,7 +23,7 @@ const ShowBalanceAdmin = () =>{
         }
     }
     const fetchBalanceAccId = () => {
-        const URL = `http://localhost:8080/checkBalanceByAccNo/${accountId.target.value}`
+        const URL = `http://localhost:8080/checkBalanceByAccNo/${accountId}`
         axios({
             method: 'get',
             url: URL
@@ -29,8 +33,8 @@ const ShowBalanceAdmin = () =>{
         })
     }
     const fetchBalanceCustId = () =>{
-        const URL = `http://localhost:8080/checkBalance/${custId.target.value}`
-        console.log(custId.target.value);
+        const URL = `http://localhost:8080/checkBalance/${custId}`
+        // console.log(custId.target.value);
         axios({
             method: 'get',
             url: URL,
@@ -51,22 +55,48 @@ const ShowBalanceAdmin = () =>{
 
     return (
         <div>
-            <h1>Check Balance Using Customer Id</h1>
+            <CDBContainer style={{marginLeft: "40%", marginTop: "10%"}}>
+            <CDBCard style={{ width: "35rem", borderRadius: "1rem" }} border>
+            <CDBCardBody>
+           
+            <h3  style={{padding: "6%"}}>Check Balance</h3>
+            <div className="d-flex justify-content-center">
+            <form onSubmit={(e)=>e.preventDefault()}>
+            <Container>
+                <div class="group">  
+              
             <Select
                 name='select2'
                 options={queryTypes}
                 onChange={value=>setType(value.label)}
                />
+               </div>
                {
                 type === 'customer id' ? 
 
-                <input name='custId' placeholder='Enter Customer Id' onChange={e => setCustId(e)}></input>
+                // <input name='custId' placeholder='Enter Customer Id' onChange={e => setCustId(e)}></input>
+               
+                <div class="group">  
+                    <input name='custId' required onChange={e => setCustId(e.target.value)}></input>
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+                    <label>Customer ID</label>     
+                </div>
                 :
-                <input name='accountId' placeholder='Enter Account Id' onChange={e => setAccountId(e)}></input>
+                // <input name='accountId' placeholder='Enter Account Id' onChange={e => setAccountId(e)}></input>
 
+                <div class="group">  
+                    <input name='accountId' required onChange={e => setAccountId(e.target.value)}></input>
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+                    <label>Account No.</label>     
+                </div>
                 }
+                <div class="group">
             <button onClick={handleFetch}>Check Balance</button>
-            
+            </div>
+            </Container>
+            </form>
             { account.length > 0 ? 
             <table>
                 <tbody>
@@ -86,8 +116,12 @@ const ShowBalanceAdmin = () =>{
             <div></div>
             }
             {balance !== -1 ? <div>Balance is {balance}</div> : <div></div>}
-            
+            </div>
+             </CDBCardBody>
+            </CDBCard>
+          </CDBContainer>
        </div>
+       
     )
 }
 
